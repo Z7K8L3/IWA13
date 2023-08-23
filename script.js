c44257ba4563b6f4f2215f9d953320cedf875889
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-23 22:38:49 +0200

    Added html and javascript code

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-let state = 'idle'
-let user = null
-let calculated = '1'
+let state = "idle";
+let user = null;
+let calculated = "1";
 
 // Only allowed to change below
 
+const logCalc = () => {
+  const isString = typeof calculated === "string";
+  const calculatedAsNumber = isString
+    ? parseFloat(calculated)
+    : Number(calculated);
+  return calculatedAsNumber === calculatedAsNumber - 1;
+};
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
-}
+const calcUser = () => {
+  const result = logCalc();
+  if (result && user === null) {
+    user = "John";
+    state = "requesting";
+  }
+  if (result && state === "requesting") {
+    state = "idle";
+  }
+};
 
-const = calcUser () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
-}
-
-const = checkUser () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
-}
+const checkUser = () => {
+  if (user === "John" && state === "requesting") {
+    console.log(`User: ${user} (${calculated})`);
+  }
+};
 
 // Only allowed to change code above
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
