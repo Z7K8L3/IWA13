ecc4b06387385b8617e9f73cb52b754f8c505fcf
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-24 06:04:02 +0200

    Updated the code

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
 let state = "idle";
 let user = null;
-let calculated = "1";
+let calculated = 1;
 
 // Only allowed to change below
 
 const logCalc = () => {
-  const isString = typeof calculated === "string";
-  const calculatedAsNumber = isString
-    ? parseFloat(calculated)
-    : Number(calculated);
-  return calculatedAsNumber === calculatedAsNumber - 1;
+  const isString = typeof calculated === "string" && !isNaN(calculated);
+  const calculatedAsNumber = isString ? parseInt(calculated) : calculated;
+  calculated = calculatedAsNumber + 1;
 };
 
 const calcUser = () => {
-  const result = logCalc();
-  if (result && user === null) {
-    user = "John";
-    state = "requesting";
-  }
-  if (result && state === "requesting") {
-    state = "idle";
-  }
+  logCalc();
+  if (calculated > 2) user = "John";
+  if (calculated > 2) state = "requesting";
+  if (calculated > 3) state = "idle";
 };
 
 const checkUser = () => {
-  if (user === "John" && state === "requesting") {
+  if (user && state === "requesting") {
     console.log(`User: ${user} (${calculated})`);
   }
 };
 
 // Only allowed to change code above
 
-checkUser();
 calcUser();
 
 checkUser();
+
 calcUser();
 
 checkUser();
+
 calcUser();
 
 checkUser();
+
 calcUser();
 
 checkUser();
+
 calcUser();
+
+checkUser();
